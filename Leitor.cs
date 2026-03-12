620f7703ce6af391bc623ea2fbfe2be2b13df762
Gustavo <gustavo2005@uniplaclages.edu.br>
2026-03-12 15:16:54 -0300

    Criação da Listagem e refinamento de outras funções

--- Leitor.cs
+++ Leitor.cs
@@ -21,7 +21,7 @@ public class Leitor
         leitorDestino.AdiconarLivro(livroDoado);
     }
 
-    public Leitor cadastroLeitor()
+    public Leitor cadastroLeitor(List<Leitor> listaDeLeitores)
     {
         Leitor novoLeitor = new Leitor();
         Console.WriteLine("\n---Cadastro de Leitor---");
@@ -29,22 +29,55 @@ public class Leitor
         while (true) //Validação do CPF para garantir que o identificador tenha exatamente 11 dígitos
         {
             Console.Write("Digite o CPF do leitor (11 dígitos, sem pontuação): ");
-            novoLeitor.Cpf = Console.ReadLine();
-            if (!string.IsNullOrEmpty(novoLeitor.Cpf) && novoLeitor.Cpf.Length == 11)
+            string cpfDigitado = Console.ReadLine();
+
+
+            if (!string.IsNullOrEmpty(cpfDigitado) && cpfDigitado.Length == 11)
             {
-                break;
+
+                bool cpfInvalido = listaDeLeitores.Exists(l => l.Cpf == cpfDigitado);
+                if (cpfInvalido)
+                {
+                    Console.WriteLine("CPF já cadastrado! Por favor, insira um CPF diferente.");
+                    continue;
+                }
+                else
+                {
+                    novoLeitor.Cpf = cpfDigitado;
+                    break;
+                }
             }
             Console.WriteLine("CPF Inválido! O identificador deve ter exatamente 11 números.");
         }
 
-        Console.Write("Digite o nome do leitor: ");
-        novoLeitor.Nome = Console.ReadLine();
+        while (true)
+        {
+            Console.Write("Digite o nome do leitor: ");
+            string entrada = Console.ReadLine();
 
-        Console.Write("Digite a idade do leitor: ");
-        byte.TryParse(Console.ReadLine(), out byte idade);
-        novoLeitor.Idade = idade;
+            if (!string.IsNullOrWhiteSpace(entrada))
+            {
+                novoLeitor.Nome = entrada;
+                break;
+            }
+            Console.WriteLine("O nome não pode estar vazio!");
+        }
 
-        while (true) //Validação do contato para garantir que o número de telefone tenha exatamente 13 caracteres (incluindo espaço e hífen)
+        while (true)
+        {
+            Console.Write("Digite a idade do leitor: ");
+            byte.TryParse(Console.ReadLine(), out byte idade);
+
+            if (idade <= 0 || idade == null)
+            {
+                Console.WriteLine("Insira um valor válido.");
+                continue;
+            }
+            novoLeitor.Idade = idade;
+            break;
+        }
+
+        while (true)
         {
             Console.Write("Digite o contato do leitor (xx xxxxx-xxxx): ");
             novoLeitor.Contato = Console.ReadLine();
@@ -55,5 +88,36 @@ public class Leitor
             Console.WriteLine("Contato escrito de forma errada.");
         }
         return novoLeitor;
+    }
+
+    public void listarLeitores(List<Leitor> listaDeLeitores)
+    {
+        if (listaDeLeitores.Count == 0)
+        {
+            Console.WriteLine("\nNenhum leitor cadastrado.");
+            return;
+        }
+
+        Console.WriteLine("\n-- Todos os leitores cadastrados --");
+        foreach (var leitor in listaDeLeitores)
+        {
+            leitor.ExibirDetalhes();
+        }
+    }
+
+    public void ExibirDetalhes() // Listagem de leitor específico
+    {
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine($"Nome: {this.Nome}");
+        Console.WriteLine($"CPF: {this.Cpf} || Idade: {this.Idade}");
+        Console.WriteLine($"Contato: {this.Contato}");
+        if (LivrosLeitor.Count > 0)
+        {
+            Console.WriteLine($"Livros: {string.Join(", ", LivrosLeitor.Select(l => l.Titulo))}");
+        }
+        else
+        {
+            Console.WriteLine("Livros: Nenhum livro.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,10 +9,10 @@ internal class Program
         while (true)
         {
             Menu menu = new Menu();
+
             Console.WriteLine("\nDigite o numero da sua opção");
             menu.ListarOpcoes();
             short opcaoEscolhida;
-
             Console.Write("Escolha uma opção: ");
 
             if (!short.TryParse(Console.ReadLine(), out opcaoEscolhida))
@@ -25,21 +25,55 @@ internal class Program
             {
                 case 1: //Leitor
                     Console.WriteLine("\n---Você escolheu a opção de leitores---");
+
                     Console.WriteLine("Digite o numero da sua opção");
                     menu.ListarOpcoesLeitor();
                     Console.Write("Escolha uma opção: ");
-                    
+
                     if (short.TryParse(Console.ReadLine(), out short opcaoOperacao))
                     {
                         if (opcaoOperacao == 1) //Adicionar novo leitor
                         {
-                           Leitor leitorCadastrado = new Leitor().cadastroLeitor();
-                           leitores.Add(leitorCadastrado);
-                           Console.WriteLine($"\n{leitorCadastrado.Nome} cadastrado com sucesso!");
+                            Leitor leitorCadastrado = new Leitor().cadastroLeitor(leitores);
+                            leitores.Add(leitorCadastrado);
+                            Console.WriteLine($"\n{leitorCadastrado.Nome} cadastrado com sucesso!");
                         }
 
                         if (opcaoOperacao == 2)//Listar leitores
                         {
+                            Console.WriteLine("\n--- Submenu de Listagem ---");
+                            Console.WriteLine("1 - Listar TODOS os leitores");
+                            Console.WriteLine("2 - Buscar leitor específico (por CPF)");
+                            Console.Write("Escolha uma opção: ");
+
+                            if (short.TryParse(Console.ReadLine(), out short opcaoListagem))
+                            {
+                                if (opcaoListagem == 1) // Listar Todos
+                                {
+                                    new Leitor().listarLeitores(leitores);
+                                }
+                                
+                                else if (opcaoListagem == 2) // Listar específico
+                                {
+                                    Console.Write("\nDigite o CPF do leitor que deseja buscar: ");
+                                    string cpfBusca = Console.ReadLine();
+                                    Leitor leitorEncontrado = leitores.Find(l => l.Cpf == cpfBusca);
+
+                                    if (leitorEncontrado != null)
+                                    {
+                                        Console.WriteLine("\n--- Detalhes do Leitor Encontrado ---");
+                                        leitorEncontrado.ExibirDetalhes();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Leitor não encontrado com o CPF fornecido.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Opção de listagem inválida.");
+                                }
+                            }
                         }
 
                         break;
