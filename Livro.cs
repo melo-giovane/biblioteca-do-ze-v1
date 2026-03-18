6ceac1bc595c0d7fb25bf56bffc32693b9f536ba
Gustavo <gustavo2005@uniplaclages.edu.br>
2026-03-17 22:34:30 -0300

    Implementar funcionalidades de edição e cadastro de leitores e livros

--- Livro.cs
+++ Livro.cs
@@ -31,4 +31,97 @@ public int NumeroDePaginas;
         TipoDaCapa = tipoDaCapa;
         NumeroDePaginas = numeroDePaginas;
     }
+
+    public Livro CadastroLivro()
+{
+    Console.WriteLine("\n--- Cadastro de Livro ---");
+
+    Console.Write("Título: ");
+    string titulo = Console.ReadLine() ?? "";
+
+    Console.Write("Subtítulo: ");
+    string subTitulo = Console.ReadLine() ?? "";
+
+    Console.Write("Escritor: ");
+    string escritor = Console.ReadLine() ?? "";
+
+    Console.Write("Editora: ");
+    string editora = Console.ReadLine() ?? "";
+
+    Console.Write("Gênero: ");
+    string genero = Console.ReadLine() ?? "";
+
+    Console.Write("Ano de Publicação: ");
+    int.TryParse(Console.ReadLine(), out int ano);
+
+    Console.Write("Tipo da Capa: ");
+    string capa = Console.ReadLine() ?? "";
+
+    Console.Write("Número de Páginas: ");
+    int.TryParse(Console.ReadLine(), out int paginas);
+
+    return new Livro(titulo, subTitulo, escritor, editora, genero, ano, capa, paginas);
+}
+
+public void editarLivro()
+    {
+        Console.WriteLine("Digite o que deseja alterar:");
+        Console.WriteLine("1 - Título");
+        Console.WriteLine("2 - Subtítulo");
+        Console.WriteLine("3 - Escritor");
+        Console.WriteLine("4 - Editora");
+        Console.WriteLine("5 - Gênero");
+        Console.WriteLine("6 - Ano de Publicação");
+        Console.WriteLine("7 - Tipo da Capa");
+        Console.WriteLine("8 - Número de Páginas");
+
+        if (!int.TryParse(Console.ReadLine(), out int opcao))
+        {
+            Console.WriteLine("Digite um número válido.");
+            return;
+        }
+
+        switch (opcao)
+        {
+            case 1:
+                Console.Write("Novo Título: ");
+                this.Titulo = Console.ReadLine() ?? this.Titulo;
+                break;
+            case 2:
+                Console.Write("Novo Subtítulo: ");
+                this.SubTitulo = Console.ReadLine() ?? this.SubTitulo;
+                break;
+            case 3:
+                Console.Write("Novo Escritor: ");
+                this.Escritor = Console.ReadLine() ?? this.Escritor;
+                break;
+            case 4:
+                Console.Write("Nova Editora: ");
+                this.Editora = Console.ReadLine() ?? this.Editora;
+                break;
+            case 5:
+                Console.Write("Novo Gênero: ");
+                this.Genero = Console.ReadLine() ?? this.Genero;
+                break;
+            case 6:
+                Console.Write("Novo Ano de Publicação: ");
+                int.TryParse(Console.ReadLine(), out int ano);
+                if (ano > 0)
+                    this.AnoPublicacao = ano;
+                break;
+            case 7:
+                Console.Write("Novo Tipo da Capa: ");
+                this.TipoDaCapa = Console.ReadLine() ?? this.TipoDaCapa;
+                break;
+            case 8:
+                Console.Write("Novo Número de Páginas: ");
+                int.TryParse(Console.ReadLine(), out int paginas);
+                if (paginas > 0)
+                    this.NumeroDePaginas = paginas;
+                break;
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,40 +1,14 @@
-﻿using Biblioteca;
+﻿//GUSTAVO DECKER COUTO
+//GIOVANE MELO
+using Biblioteca;
 
 internal class Program
 {
     static List<Leitor> leitores = new List<Leitor>();
+    static List<Livro> livros = new List<Livro>();
     static void Main(string[] args)
     {
         Menu menu = new Menu();
-
-        while (true)
-        {
-            short opcaoPrincipal = menu.MenuPrincipal();
-
-            switch (opcaoPrincipal)
-            {
-                case 1: // Leitor
-                    MenuLeitor(menu);
-                    break;
-
-                case 2: // Livro
-                    MenuLivro(menu);
-                    break;
-
-                case 3: // Encerrar
-                    Console.WriteLine("Encerrando o programa...");
-                    Environment.Exit(0);
-                    break;
-
-                default:
-                    Console.WriteLine("Opção inválida.");
-                    break;
-            }
-        }
-    }
-
-    static void MenuLeitor(Menu menu)
-    {
         while (true)
         {
             short opcao = menu.MenuLeitor();
@@ -49,25 +23,124 @@ internal class Program
                     break;
 
                 case 2:
+                    //Listar todos os leitores
                     Leitor x = new Leitor();
                     Leitor.ListarLeitores(leitores);
                     break;
 
                 case 3:
-                    Console.WriteLine("Editar leitor");
+                    //Exibir leitor específico
+                    Console.WriteLine("Exibir leitor específico:");
+                    Console.WriteLine("Digite o cpf do leitor que deseja exibir:");
+                    string cpfLeitor = Console.ReadLine();
+                    Leitor? leitor = leitores.Find(x => x.cpf == cpfLeitor);
+                    if (leitor! != null)
+                    {
+                        leitor.ExibirDetalhes();
+                    }
                     break;
 
                 case 4:
-                    Console.WriteLine("Remover leitor");
+                    Console.WriteLine("Editar leitor");
+                    Console.WriteLine("Digite o cpf do leitor que deseja editar:");
+                    string leitorEditar = Console.ReadLine();
+                    Leitor? editar = leitores.Find(x => x.cpf == leitorEditar);
+                    if (editar! != null)
+                    {
+                        editar.editarLeitor();
+                    }
                     break;
 
                 case 5:
+                    Console.WriteLine("Excluir leitor");
+                    Console.WriteLine("Digite o cpf do leitor que deseja excluir:");
+                    string cpfExcluir = Console.ReadLine();
+                    Leitor? excluir = leitores.Find(x => x.cpf == cpfExcluir);
+                    if (excluir! != null)
+                    {
+                        leitores.Remove(excluir);
+                        Console.WriteLine("Leitor excluído com sucesso!");
+                    }
+                    break;
+
+                case 6:
+                    Console.WriteLine("Digite o cpf do leitor");
+                    string dono = Console.ReadLine();
+                    Leitor? donoLeitor = leitores.Find(x => x.cpf == dono);
+                    if (donoLeitor! != null)
+                    {
+                        Console.WriteLine("Incluir livro");
+                        Livro novoLivro = new Livro().CadastroLivro();
+                        donoLeitor.LivrosLeitor.Add(novoLivro);
+                        Console.WriteLine("Livro cadastrado com sucesso!");
+                    }
+                    break;
+
+                case 7:
+                    Console.WriteLine("Editar livro");
+                    Console.WriteLine("Digite o cpf do leitor que possui o livro:");
+                    string cpfPropietario = Console.ReadLine();
+                    Leitor? proprietario = leitores.Find(x => x.cpf == cpfPropietario);
+                    if (proprietario! != null)
+                    {
+                        Console.WriteLine("Digite o título do livro que deseja editar:");
+                        string tituloLivro = Console.ReadLine();
+                        Livro? livroEditar = proprietario.LivrosLeitor.Find(x => x.Titulo == tituloLivro);
+                        if (livroEditar! != null)
+                        {
+                            livroEditar.editarLivro();
+                            Console.WriteLine("Livro editado com sucesso!");
+                        }
+                    }
+                    break;
+
+                case 8:
+                    Console.WriteLine("Doar livro");
                     Console.WriteLine("Digite o cpf do doador");
                     string cpfDoador = Console.ReadLine();
                     Leitor? doador = leitores.Find(x => x.cpf == cpfDoador);
-                    if(doador! != null)
+                    if (doador! != null)
                     {
                         doador.DoarLivro(leitores);
+                    }
+                    break;
+
+                case 9:
+                    Console.WriteLine("Remover livro");
+                    Console.WriteLine("Digite o cpf do leitor que possui o livro:");
+                    string cpf3 = Console.ReadLine();
+                    Leitor? proprietarioRemover = leitores.Find(x => x.cpf == cpf3);
+                    if (proprietarioRemover! != null)
+                    {
+                        Console.WriteLine("Digite o título do livro que deseja remover:");
+                        string tituloLivro = Console.ReadLine();
+                        Livro? livroRemover = proprietarioRemover.LivrosLeitor.Find(x => x.Titulo == tituloLivro);
+                        if (livroRemover! != null)
+                        {
+                            proprietarioRemover.LivrosLeitor.Remove(livroRemover);
+                            Console.WriteLine("Livro removido com sucesso!");
+                        }
+                    }
+                    break;
+
+                case 10:
+                    Console.WriteLine("Procurar livro");
+                    Console.WriteLine("Digite o título do livro que deseja procurar:");
+                    string tituloProcurar = Console.ReadLine();
+                    Console.WriteLine($"Título: {tituloProcurar} - Leitores:");
+                    foreach (Leitor l in leitores)
+                    {
+                        foreach (Livro livro in l.LivrosLeitor)
+                        {
+                            if (livro.Titulo == tituloProcurar)
+                            {
+                               Console.WriteLine($"- {l.nome} || CPF: {l.cpf} || Idade: {l.idade} || Contato: {l.contato}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Livro não encontrado.");
+                            }
+                        }
                     }
                     break;
 
@@ -80,40 +153,43 @@ internal class Program
             if (Console.ReadLine() == "0")
                 break;
         }
-    }
 
-    static void MenuLivro(Menu menu)
-    {
-        while (true)
+        static void MenuLivro(Menu menu)
         {
-            short opcao = menu.MenuLivro();
-
-            switch (opcao)
+            while (true)
             {
-                case 1:
-                    Console.WriteLine("Adicionar livro");
-                    break;
+                short opcao = menu.MenuLivro();
 
-                case 2:
-                    Console.WriteLine("Listar livros");
-                    break;
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Adicionar livro");
+                        Livro novoLivro = new Livro().CadastroLivro();
+                        livros.Add(novoLivro);
+                        Console.WriteLine("Livro cadastrado com sucesso!");
+                        break;
 
-                case 3:
-                    Console.WriteLine("Editar livro");
-                    break;
+                    case 2:
+                        Console.WriteLine("Listar livros");
+                        break;
 
-                case 4:
-                    Console.WriteLine("Remover livro");
-                    break;
+                    case 3:
+                        Console.WriteLine("Editar livro");
+                        break;
 
-                default:
-                    Console.WriteLine("Opção inválida.");
+                    case 4:
+                        Console.WriteLine("Remover livro");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+
+                Console.WriteLine("\nDigite 0 para voltar ou qualquer outra tecla para continuar...");
+                if (Console.ReadLine() == "0")
                     break;
             }
-
-            Console.WriteLine("\nDigite 0 para voltar ou qualquer outra tecla para continuar...");
-            if (Console.ReadLine() == "0")
-                break;
         }
     }
 }
